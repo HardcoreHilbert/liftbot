28809f30baa37a37b014820a973c6cdf4ff810cd
HardcoreHilbert <chilbert2206@gmail.com>
2022-08-12 23:20:38 -0400

    added authorization ability

--- src/main/java/com/barbenders/liftbot/app/LiftbotApp.java
+++ src/main/java/com/barbenders/liftbot/app/LiftbotApp.java
@@ -1,15 +1,27 @@
 package com.barbenders.liftbot.app;
 
 import com.slack.api.bolt.App;
+import com.slack.api.bolt.AppConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LiftbotApp {
+
     @Bean
-    public App initLiftbotApp(){
-        App liftbotApp = new App();
+    public AppConfig loadSingleWorkspaceAppConfig() {
+        return AppConfig.builder()
+                .singleTeamBotToken(System.getenv("SLACK_BOT_TOKEN"))
+                .signingSecret(System.getenv("SLACK_SIGNING_SECRET"))
+                .build();
+    }
+
+    @Bean
+    public App initLiftbotApp(AppConfig config){
+        App liftbotApp = new App(config);
+
         liftbotApp.command("/hello", (slashCommandRequest, context) -> context.ack("BeepBoop here comes the joop"));
+
         return liftbotApp;
     }
 }
