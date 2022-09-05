b688b3ca7a1c0874cc04743d99da191160368d20
HardcoreHilbert <chilbert2206@gmail.com>
2022-09-04 23:13:58 -0400

    checking out payload of save button press

--- src/main/java/com/barbenders/liftbot/app/LiftbotApp.java
+++ src/main/java/com/barbenders/liftbot/app/LiftbotApp.java
@@ -5,12 +5,13 @@ import com.barbenders.liftbot.repo.ExerciseRepository;
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.AppConfig;
 import com.slack.api.methods.request.views.ViewsPublishRequest;
-import com.slack.api.model.block.HeaderBlock;
-import com.slack.api.model.block.LayoutBlock;
-import com.slack.api.model.block.SectionBlock;
+import com.slack.api.model.block.*;
 import com.slack.api.model.block.composition.MarkdownTextObject;
 import com.slack.api.model.block.composition.PlainTextObject;
 import com.slack.api.model.block.composition.TextObject;
+import com.slack.api.model.block.element.BlockElement;
+import com.slack.api.model.block.element.ButtonElement;
+import com.slack.api.model.block.element.UsersSelectElement;
 import com.slack.api.model.event.AppHomeOpenedEvent;
 import com.slack.api.model.view.View;
 import com.slack.api.model.view.ViewState;
@@ -52,10 +53,66 @@ public class LiftbotApp {
         App liftbotApp = new App(config);
 
 
-        initAddExerciseHomeView(liftbotApp);
-        initSaveAction(liftbotApp);
+        initHomeView(liftbotApp);
+        //initAddExerciseHomeView(liftbotApp);
+        //initSaveAction(liftbotApp);
 
         return liftbotApp;
+    }
+
+    private void initHomeView(App liftBot) {
+        LOGGER.info("initializing home screen view");
+        liftBot.event(AppHomeOpenedEvent.class, (request, context) -> {
+            String userId = request.getEvent().getUser();
+            LOGGER.info("user that opened home view: {}", userId);
+            try {
+                View homeLanding = View.builder()
+                        .type("home")
+                        .blocks(createHomeLanding(userId))
+                        .build();
+
+                ViewsPublishRequest addView = ViewsPublishRequest.builder()
+                        .view(homeLanding)
+                        .token(System.getenv("SLACK_BOT_TOKEN"))
+                        .userId(userId)
+                        .build();
+                context.client().viewsPublish(addView);
+            } catch (Exception e) {
+                LOGGER.error("Exception: ", e);
+            }
+            return context.ack();
+        });
+    }
+
+    private List<LayoutBlock> createHomeLanding(String userId) {
+        List<LayoutBlock> blocks = new ArrayList<>();
+
+        SectionBlock sectionBlock = SectionBlock.builder()
+                .text(MarkdownTextObject.builder().text("choose user").build())
+                .accessory(UsersSelectElement.builder().actionId("selected_user").build())
+                .build();
+
+        ActionsBlock actionsBlock = ActionsBlock.builder()
+                .elements(new ArrayList<BlockElement>(){
+                    {
+                        add(ButtonElement.builder()
+                                .actionId("view_records")
+                                .text(PlainTextObject.builder().text("View Workout Records").build())
+                                .build());
+                        add(ButtonElement.builder()
+                                .actionId("add_record")
+                                .text(PlainTextObject.builder().text("Add New Record").build())
+                                .build());
+                    }
+                })
+                .build();
+
+        blocks.add(new DividerBlock());
+        blocks.add(sectionBlock);
+        blocks.add(new DividerBlock());
+        blocks.add(actionsBlock);
+
+        return blocks;
     }
 
     private void initAddExerciseHomeView(App liftbotApp) {
