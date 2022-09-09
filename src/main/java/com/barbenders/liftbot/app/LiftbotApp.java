1b6151e6a67f1cb3c8b1afc31e1a87e0fa3ef962
HardcoreHilbert <chilbert2206@gmail.com>
2022-09-09 16:43:56 -0400

    creating back button

--- src/main/java/com/barbenders/liftbot/app/LiftbotApp.java
+++ src/main/java/com/barbenders/liftbot/app/LiftbotApp.java
@@ -148,7 +148,7 @@ public class LiftbotApp {
             try {
                 Double.parseDouble(record.getWeight());
             } catch (NumberFormatException e) {
-                String error = "{\"response_action\":\"errors\",\"errors\": {\"weight_input\": \"weight must be a number\"}}";
+                String error = "{response_action:errors,errors:{weight_input:weight must be a number}}";
                 return context.ack(context.toJson(error));
             }
             repo.save(record);
